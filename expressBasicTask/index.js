7375b20319fe1214efe131d912b253a08d5511c7
MD Juber Akhtar <vastulorde99@gmail.com>
2020-11-06 10:50:45 +0530

    first commit

--- expressBasicTask/index.js
+++ expressBasicTask/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
-const fs = require('fs')
+const fs = require('fs');
+const AppError = require('./utils/appError');
 
 const app = express();
-
-app.use(express.json());
 
 const students = JSON.parse(fs.readFileSync('./student.json'));
 
 //middleware
+app.use(express.json());
+
 const checkValue= (req, res, next)=>{
-    if(!req.body.name || !req.body.age){
+    if(!req.body.name || !req.body.age || !req.body.gender){
         return res.status(404).json({
             status: 'fail',
             message: 'Incomplete Information'
@@ -19,9 +20,20 @@ const checkValue= (req, res, next)=>{
 
 }
 
+const checkQuery= (req, res, next)=>{
+    if(!req.query.length || !req.params.length){
+        return res.status(404).json({
+            status: 'success',
+            message: 'To get more information provide additional details'
+          });
+    }
+    next();
+
+}
+
 
 //get all students
-app.get('/students', (req, res)=>{
+app.get('/students', (req, res, next)=>{
     res.status(200).json({
         status: 'success',
         results: students.length,
@@ -33,16 +45,14 @@ app.get('/students', (req, res)=>{
 });
 
 //get info through name params
-app.get('/student/:name',  (req, res)=>{
+app.get('/student/:name', checkQuery, (req, res, next)=>{
     const { name } = req.params;
     const details = students.filter(x=> x.name.toLowerCase() === name)
 
-    if(details.length === 0){ 
-        res.status(404).json({
-            status:'fail',
-            message:'Sorry! No one is there by that name'
-        })
-    }
+    if (details.length === 0) {
+        return next(new AppError('No student found with that name', 404));
+      }
+
     res.status(200).json({
         status:'success',
         data:{
@@ -53,28 +63,24 @@ app.get('/student/:name',  (req, res)=>{
 });
 
 //get info through age query or gender
-app.get('/student', (req, res)=>{
+app.get('/student', checkQuery, (req, res, next)=>{
     const { age, gender} = req.query;
-
-    const detail = students.filter(x=> x.age === age ||  x.gender == gender);
+    
+    const detail = students.filter(x=> x.age === age || x.gender === gender);
+    if (detail.length === 0) {
+        return next(new AppError('No student found with that details', 404));
+      }
   
-    if(detail.length === 0){ 
-        res.status(404).json({
-            status:'fail',
-            message:'Sorry! No one by that age'
+     res.status(200).json({
+            status:"success",
+            data:{
+                detail
+            }
         })
-    }
-
-    res.status(200).json({
-        status:"success",
-        data:{
-            detail
-        }
-    })
 });
 
 //add new student to the json
- app.post('/student', checkValue, (req, res)=>{
+ app.post('/student', checkValue, (req, res, next)=>{
     const newStudent = Object.assign(req.body);
   
     students.push(newStudent);
